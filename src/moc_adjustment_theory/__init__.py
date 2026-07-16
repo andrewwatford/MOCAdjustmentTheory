c7198c01a9e36e25da4f6098fa531f579e4f251f
Andrew Watford <84861715+andrewwatford@users.noreply.github.com>
2026-07-16 16:53:08 +0100

    Implement frequency-domain adjustment model

--- src/moc_adjustment_theory/__init__.py
+++ src/moc_adjustment_theory/__init__.py
@@ -1,6 +1,7 @@
 """Modular theory of meridional-overturning-circulation adjustment."""
 
+from .model import GlobalAdjustmentModel
 from .hello import hello_world
 
-__all__ = ["hello_world"]
+__all__ = ["GlobalAdjustmentModel", "hello_world"]
 __version__ = "0.1.0"
