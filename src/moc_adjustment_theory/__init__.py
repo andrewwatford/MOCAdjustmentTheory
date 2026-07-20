26d5c13eb3feaecc9dbd7c384fd8d6c3209127af
andrewwatford <andrew.watford@dtc.ox.ac.uk>
2026-07-20 09:31:17 +0100

    exposing physical constants

--- src/moc_adjustment_theory/__init__.py
+++ src/moc_adjustment_theory/__init__.py
@@ -1,12 +1,14 @@
 """Modular theory of meridional-overturning-circulation adjustment."""
 
 from .fourier import butterworth_filter, forward_transform, inverse_transform
-from .global_rossby import GlobalRossbyModel
+from .global_rossby import GlobalRossbyModel, EARTH_RADIUS_M, EARTH_ROTATION_S
 
 __all__ = [
     "GlobalRossbyModel",
     "butterworth_filter",
     "forward_transform",
     "inverse_transform",
+    "EARTH_RADIUS_M",
+    "EARTH_ROTATION_S",
 ]
 __version__ = "0.1.0"
